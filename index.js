6195f6bb81f454278b798654ecb2338f5c8e61df
Luka Saldivia <saldivialuka@gmail.com>
2025-04-19 18:10:51 -0300

    refactor: included Queryable on export

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Model } from "./src/Model/Model"
 import { AggregateFunction } from "./src/AggregateFunction/AggregateFunction"
 import { TableFactory } from "./src/Table/TableFactory"
+import { Queryable } from "./src/Model/types"
 
-export default { Model, AggregateFunction, TableFactory}
+export default { Model, AggregateFunction, TableFactory, Queryable}
