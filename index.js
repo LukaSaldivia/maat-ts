162277b279ef159871169fb6d504eff45e9093a8
Luka Saldivia <saldivialuka@gmail.com>
2025-04-19 18:15:43 -0300

    refactor: index.js to index.ts

--- index.js
+++
@@ -1,6 +0,0 @@
-import { Model } from "./src/Model/Model"
-import { AggregateFunction } from "./src/AggregateFunction/AggregateFunction"
-import { TableFactory } from "./src/Table/TableFactory"
-import { Queryable } from "./src/Model/types"
-
-export default { Model, AggregateFunction, TableFactory, Queryable}
